3ddea5dc651b23c45c2497e3eb0eabbf18f781fc
Ilia Bykov <fagotist96@icloud.com>
2024-01-12 17:46:30 +0300

    создал роуты

--- packages/client/src/components/Router/Router.tsx
+++ packages/client/src/components/Router/Router.tsx
@@ -12,15 +12,19 @@ import Forum from "../Forum/Forum";
 import Game from "../Game/Game";
 import LiderBoard from "../LiderBoard/LiderBoard";
 import NotFoundPage from "../404/404";
+import Profile from "../../Profile/Profile";
+import Main from "../Main/Main";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index path="" element={<Login />} />
+      <Route path="login" element={<Login />} />
       <Route path="registration" element={<Registration />} />
       <Route path="forum" element={<Forum />} />
       <Route path="game" element={<Game />} />
       <Route path="lider-board" element={<LiderBoard />} />
+      <Route path="profile" element={<Profile />} />
+      <Route index path="" element={<Main />} />
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
@@ -28,15 +32,18 @@ const router = createBrowserRouter(
 
 
 // этот компонент в последсвии удалится
+// страница топика форума будет по айди скорее всего
 function Layout () {
   return (
     <>
       <nav>
-        <Link to="/">Login</Link>&nbsp;&nbsp;
-        <Link to="/registration">Registration</Link>&nbsp;&nbsp;
-        <Link to="/forum">Forum</Link>&nbsp;&nbsp;
-        <Link to="/game">Game</Link>&nbsp;&nbsp;
-        <Link to="/lider-board">LiderBoard</Link>&nbsp;&nbsp;
+        <Link to="/login">Login Page</Link>&nbsp;&nbsp;
+        <Link to="/registration">Registration Page</Link>&nbsp;&nbsp;
+        <Link to="/forum">Forum Page</Link>&nbsp;&nbsp;
+        <Link to="/game">Game Page</Link>&nbsp;&nbsp;
+        <Link to="/lider-board">LiderBoard Page</Link>&nbsp;&nbsp;
+        <Link to="/">Main Page</Link>&nbsp;&nbsp;
+        <Link to="/profile">Profile Page</Link>&nbsp;&nbsp;
       </nav>
       <Outlet />
     </>
