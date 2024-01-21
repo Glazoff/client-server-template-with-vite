79cd88ddae820ec74f61ab3ba1cc43ad240ecd9c
Ilia Bykov <fagotist96@icloud.com>
2024-01-21 22:33:39 +0300

    App.test

--- packages/client/src/App/App.test.tsx
+++ packages/client/src/App/App.test.tsx
@@ -1,15 +1,13 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-import GameMainPage from '../pages/Game'
-import { render, screen } from '@testing-library/react'
-
-const appContent = 'Начать игру'
-
 // @ts-ignore
 global.fetch = jest.fn(() =>
   Promise.resolve({ json: () => Promise.resolve('hey') })
 )
 
+function sum(a: number, b: number) {
+  return a + b
+}
+
 test('Example test', async () => {
-  render(<GameMainPage />)
-  expect(screen.getByText(appContent)).toBeDefined()
+  expect(sum(1, 2)).toEqual(3)
 })
