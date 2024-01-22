2632dd1f260170275789f18f4c70181c84d15360
Kirill Karpov <rin.veber@gmail.com>
2024-01-23 00:32:07 +0400

    fix errors

--- packages/client/src/shared/button/index.tsx
+++ packages/client/src/shared/button/index.tsx
@@ -1,13 +1,21 @@
 import styles from './style.module.scss';
 
 interface Props {
-  variant?: string
-  label: string
-  class?: string
+  variant?: string;
+  label: string;
+  class?: string;
 }
 
-export default function Button (props: Props) {
+export default function Button(props: Props) {
   return (
-    <button className={props.class + " " + (props.variant === "blue" ? styles.button : `${styles.button_blue} ${styles.button}`)}>{props.label}</button>
-  )
+    <button
+      className={
+        props.class +
+        ' ' +
+        (props.variant === 'blue' ? styles.button : `${styles.button_blue} ${styles.button}`)
+      }
+    >
+      {props.label}
+    </button>
+  );
 }
