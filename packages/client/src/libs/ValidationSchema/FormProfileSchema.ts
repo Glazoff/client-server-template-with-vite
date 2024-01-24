db587ee6e9bf08c065c217719436b0930ae93b2e
Kirill Karpov <rin.veber@gmail.com>
2024-01-24 12:12:14 +0400

    fix validation text

--- packages/client/src/libs/ValidationSchema/FormProfileSchema.ts
+++ packages/client/src/libs/ValidationSchema/FormProfileSchema.ts
@@ -13,15 +13,15 @@ const regexpPhone = /^(\+)?[\d]{10,15}$/;
 export const FormProfileSchema: yup.ObjectSchema<FormProfileType> = yup.object({
   first_name: yup
     .string()
-    .min(2, 'Минимум 5 букв')
+    .min(2, 'Минимум 2 букв')
     .matches(regexpName, '1 буква должна быть заглавной'),
   second_name: yup
     .string()
-    .min(2, 'Минимум 5 букв')
+    .min(2, 'Минимум 2 букв')
     .matches(regexpName, '1 буква должна быть заглавной'),
   login: yup
     .string()
-    .min(3, 'Минимум 2 букв')
+    .min(3, 'Минимум 3 букв')
     .max(20, 'Максимум 20 букв')
     .matches(regexpLogin, 'Должна быть буква, заглавная буква цифра. Только латтиница'),
   email: yup.string().email('Укажите почту в формате you@yandex.ru'),
