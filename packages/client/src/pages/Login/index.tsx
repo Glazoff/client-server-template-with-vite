a8d4adbd0bc0026abe4af89d476ab627dc49a457
Kirill Karpov <rin.veber@gmail.com>
2024-01-24 14:12:58 +0400

    add mui theme

--- packages/client/src/pages/Login/index.tsx
+++ packages/client/src/pages/Login/index.tsx
@@ -1,3 +1,11 @@
+import { Button } from '@mui/material';
+
 export default function Login() {
-  return <h1>Страница авторизации</h1>;
+  return (
+    <h1>
+      Страница авторизации
+      <Button variant={'default'}>Кнопка</Button>
+      <Button variant="orange">Кнопка</Button>
+    </h1>
+  );
 }
