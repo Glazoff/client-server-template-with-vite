bc6e7ecbf08193d210b7af9f9f2cfd60a5c4c919
kalikrit <kalikrit@gmail.com>
2024-01-24 18:18:20 +0300

    компонент авторизации и регистрации

--- packages/client/src/pages/Login/index.tsx
+++ packages/client/src/pages/Login/index.tsx
@@ -1,13 +1,44 @@
-import { Button } from '@mui/material';
+import { Box, TextField, Button, Typography } from '@mui/material';
 
 export default function Login() {
   return (
-    <h1>
-      Страница авторизации
-      <Button variant={'default'}>default</Button>
-      <Button variant="orange">orange</Button>
-      <Button variant="hollow">hollow</Button>
-      <Button variant="link">link</Button>
-    </h1>
+    <div className="root">
+      <form className="form" style={{ marginTop: 100 }}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          maxWidth={400}
+          margin="auto"
+          padding={5}
+          borderRadius={15}
+          border={'6px solid #00A3FF'}
+        >
+          <Typography>Авторизация</Typography>
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            label="Логин"
+            variant="outlined"
+            placeholder="Введите ваш логин"
+          />
+          <TextField
+            type="password"
+            fullWidth={true}
+            margin="normal"
+            label="Пароль"
+            variant="outlined"
+            placeholder="Введите ваш пароль"
+          />
+          <Button sx={{ marginTop: 2, marginBottom: 2, width: '50%' }} variant="contained">
+            Вход
+          </Button>
+          <Button sx={{ marginTop: 2, marginBottom: 2, width: '50%' }} variant="orange">
+            Регистрация
+          </Button>
+        </Box>
+      </form>
+    </div>
   );
 }
--- packages/client/src/pages/Registration/index.tsx
+++ packages/client/src/pages/Registration/index.tsx
@@ -1,3 +1,72 @@
+import { Box, TextField, Button, Typography } from '@mui/material';
+
 export default function Registration() {
-  return <h1>Страница регистрации</h1>;
+  return (
+    <div className="root">
+      <form className="form" style={{ marginTop: 100 }}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          maxWidth={400}
+          margin="auto"
+          padding={5}
+          borderRadius={15}
+          border={'6px solid #00A3FF'}
+        >
+          <Typography>Регистрация</Typography>
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            label="Имя"
+            variant="outlined"
+            placeholder="Введите ваше имя"
+          />
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            label="Фамилия"
+            variant="outlined"
+            placeholder="Введите вашу фамилию"
+          />
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            label="Email"
+            variant="outlined"
+            placeholder="Введите ваш email"
+          />
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            label="Телефон"
+            variant="outlined"
+            placeholder="Введите ваш телефон"
+          />
+          <TextField
+            fullWidth={true}
+            margin="normal"
+            label="Логин"
+            variant="outlined"
+            placeholder="Введите ваш логин"
+          />
+          <TextField
+            type="password"
+            fullWidth={true}
+            margin="normal"
+            label="Password"
+            variant="outlined"
+            placeholder="Введите ваш пароль"
+          />
+          <Button sx={{ marginTop: 2, marginBottom: 2, width: '50%' }} variant="contained">
+            Вход
+          </Button>
+          <Button sx={{ marginTop: 2, marginBottom: 2, width: '70%' }} variant="orange">
+            Зарегистрироваться
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 }
