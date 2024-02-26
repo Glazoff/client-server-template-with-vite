d39f3968e37560b376c667fe4fa68064f21412b0
Ilia Bykov <fagotist96@icloud.com>
2024-02-26 13:50:21 +0300

    linting

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-var-requires */
 import path from "path";
 import fsp from "fs/promises";
 import express, { Request, Response } from "express";
