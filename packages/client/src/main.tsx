50837f4b3ae1808403f4f9ae8e0dbe4f0b6352e5
kalikrit <kalikrit@gmail.com>
2024-03-12 12:14:52 +0300

    WrapperComponent

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider, createTheme } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import '../src/styles/index.scss';
 import { isServiceWorker } from './shared/serviceWorker';
-import store from './store';
+import { createStore, useAppSelector } from './store';
+import { LeaderboardStateType } from './store/liderboard/liderboardSlice';
+import { User } from './store/user/userSlice';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+const SSRState = window.__PRELOADED_STATE__;
 
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
+const state = createStore(SSRState as { leaderboard: LeaderboardStateType; user: User } | object);
+
+// eslint-disable-next-line react/display-name
+const ThemeMUIWith = (WrapperComponent: React.ComponentType) => () => {
+  const theme = useAppSelector((state) => state.theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <WrapperComponent />
+    </ThemeProvider>
+  );
+};
+
+const WrapperThemeMUI = ThemeMUIWith(App);
 
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
   <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={store.getState().theme.theme === 'light' ? lightTheme : darkTheme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+    <Provider store={state}>
+      <WrapperThemeMUI />
     </Provider>
   </React.StrictMode>
 );
--- packages/client/src/store/index.ts
+++ packages/client/src/store/index.ts
@@ -1,16 +1,20 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { leaderboardReducer } from './liderboard/liderboardSlice';
-import { themeSlice } from './theme/themeSlice';
-import userReducer from './user/userSlice';
+import { LeaderboardStateType, leaderboardReducer } from './liderboard/liderboardSlice';
+import userReducer, { User } from './user/userSlice';
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    leaderboard: leaderboardReducer,
-    theme: themeSlice.reducer,
-  },
-});
+export const createStore = (
+  initState: { leaderboard: LeaderboardStateType; user: User } | object
+) =>
+  configureStore({
+    reducer: {
+      user: userReducer,
+      leaderboard: leaderboardReducer,
+    },
+    preloadedState: initState,
+  });
+
+const store = createStore({});
 
 export type RootStore = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
